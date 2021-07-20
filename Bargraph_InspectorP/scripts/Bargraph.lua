e23e0ae55aab09c5c4c88484dc109471339345f9
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 12:37:47 +0200

    'Update'

--- Bargraph_InspectorP/scripts/Bargraph.lua
+++ Bargraph_InspectorP/scripts/Bargraph.lua
@@ -1,20 +1,3 @@
---[[----------------------------------------------------------------------------
-
-  Application Name:
-  Bargraph_InspectorP
-
-  Summary:
-  Illumination of the bargraph LED.
-
-  How to Run:
-  A connected InpspectorP device is necessary to run this sample. Starting this
-  sample is possible either by running the app (F5) or debugging (F7+F10). The
-  result can be seen by looking at the bargraph LED.
-
-  More Information:
-  See the tutorial "Audio-visual Feedback InspectorP".
-
-------------------------------------------------------------------------------]]
 
 --Start of Global Scope---------------------------------------------------------
 
